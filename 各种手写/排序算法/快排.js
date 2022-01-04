3b706c9a2e539ab6e42e181e136fbff820a64627
macmacfan <976210539@qq.com>
2022-01-05 00:35:57 +0800

    注释下快排

--- 各种手写/排序算法/快排.js
+++ 各种手写/排序算法/快排.js
@@ -16,14 +16,15 @@
 // 上边这个快排只是让读者找找感觉，我们不能这样写快排，如果每次都开两个数组，会消耗很多内存空间，数据量大时可能造成内存溢出，我们要避免开新的内存空间，即原地完成排序
 
 //  子程序的交换规则
+//  以最后一位未基准数，再设定一个初始位置pos = -1， 然后从数组第一位开始循环 每当当前项比基准小就把 pos++ 然后取arr[pos] 和当前遍历项互换
 
 function quick(arr, left = 0, right = arr.length - 1) {
   if (left < right) {
     let pos = left - 1; // 初始位置是被置换的位置，是left - 1位
     for (let i = left; i <= right; i++) {
-      let povit = arr[right]; //最后一位是基准
+      let povit = arr[right]; //取末位作基准
       if (arr[i] <= povit) {
-        //  置换
+        //  pos自增后置换当前项 和 pos指向项
         pos++;
         [arr[pos], arr[i]] = [arr[i], arr[pos]];
       }
