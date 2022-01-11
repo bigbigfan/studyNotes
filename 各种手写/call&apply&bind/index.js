0f8707e79816b6a19dfe0d244f934f5acae0b738
bigbigfan <wangzifan123@qq.com>
2022-01-11 18:07:11 +0800

    手写apply-call-bind

--- 各种手写/call&apply&bind/index.js
+++ 各种手写/call&apply&bind/index.js
@@ -50,18 +50,56 @@ function Log(name = '', profession = '') {
 // ================手写bind================
 
 Function.prototype.myBind = function (context = global, ...args) {
-   
+  if (typeof this !== "function") {
+    throw new Error("Function.prototype.bind - what is trying to be bound is not callable");
+  }  
+
    let fn = this  // 创建一个函数副本
-   return function Fn(...arguments) { //  bind后的函数副本执行的时候也可以传入参数
+  const Fn = function () { //  bind后的函数副本执行的时候也可以传入参数
     // 根据调用方式 传入不同绑定值
-    return fn.call( // 根据是否是实例判断 ? 不知为什么要判断
-      this instanceof Fn ? this : context, ...args, ...arguments 
+    return fn.apply( 
+      this instanceof Fn ? this : context, [...args, ...arguments]
     )
    }
+   Fn.prototype = Object.create(fn.prototype) // 维护原型
+   return Fn
 }
 
 // console.log(Log.bind(obj, ['wzf', 'front'])());
 // console.log(Log.myBind(obj, ['wzf', 'front'])('312'));
+
+let value = 2
+
+let foo = {
+  value: 1
+}
+
+function bar (name, age) {
+  this.habbit = 'shopping'
+  console.log(this.value);
+  console.log(name);
+  console.log(age);
+}
+
+
+bar.prototype.friend = 'kevin' // 原型上加东西了 bind之后也要保证可以访问得到 所以手写里加入了维护原型
+
+let bindFoo = bar.myBind(foo, 'disy')  
+
+var bindObj = new bindFoo('18') // bind后的函数可以作为一个构造函数调用 尽管在全局都声明了value但是 this.value -> undefined  this此时丢失了   
+
+bindFoo.prototype.friend = '3'
+console.log(bar.prototype.friend); // 原型上的属性friend被找到了。说明this绑定的对象是bar构造函数
+
+
+
+
+
+// bind 的性质是这样的 bind之后的函数如果作为了一个构造函数 被new调用 那么this是保留的 如果是普通的函数调用那么就用bind的对象的上下文
+
+
+
+
 
 
 
