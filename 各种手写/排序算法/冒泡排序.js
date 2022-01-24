523e61db0d764b908a14a505618aa4bfb545e4e3
bigbigfan <wangzifan123@qq.com>
2022-01-24 18:31:04 +0800

    冒泡改错

--- 各种手写/排序算法/冒泡排序.js
+++ 各种手写/排序算法/冒泡排序.js
@@ -1,26 +1,31 @@
-// function bable(arr) {
+function bubbleSort(arr) {
+  const l = arr.length;
+   for (let i = 0; i < l; i++) {
+    let flag = true;
+    for (let j = 0; j < l - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        flag = false;
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+      }
+    }
+    if (flag) break; // 如果flag是ture说明本次循环没有执行过冒泡，数组已经标准，直接终断循环返回结果
+  }
+  return arr;
+}
+
+// function bubbleSort(arr) {
 //   const l = arr.length;
-//   for (let i = 0; i < l; ++i) {
-//     for (let j = 0; j < l - i - 1; ++j) {
-//       let flag = true
+//   for (let i = 0; i < l; i++) {
+//     let flag = true;
+//     for (let j = 0; j < l - i - 1; j++) {
 //       if (arr[j] > arr[j + 1]) {
-//         [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
-//         flag = false
+//         flag = false;
+//         [arr[j], arr[j+1]] = [arr[j+1], arr[j]]
 //       }
-//       if(!flag) break
 //     }
+//     if (flag) break;
 //   }
 //   return arr;
 // }
-// const res = bable([1, 3, 2, 4, 6]);
-// console.log(res);
 
-let sum = 1
-for(let i = 2; i < 100; i++) { 
-  if(i % 2 === 0) {
-    sum -=  1 / i
-  } else {
-    sum += 1 / i
-  }
-}
-console.log(sum);
+console.log(bubbleSort([4, 3, 2, 5, 8, 7, 8, 7]));
